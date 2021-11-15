cee7505ca17c37a6b4d6fb4eb4a2ef0836113994
Derik van Schaik <derikvanschaik@gmail.com>
2021-11-15 00:34:40 -0800

    database added, routes refactored and model created

--- backend/index.js
+++ backend/index.js
@@ -11,15 +11,25 @@ mongoose.connect('mongodb://127.0.0.1:27017/polls');
 app.use(express.json()); 
 app.use(cors());
 
+app.get('/polls', async (req, res) =>{
+    try{
+        const polls = await Poll.find({}); 
+        res.status(200).send(polls); 
+    }catch(e){
+        res.status(500).send(e); 
+    }
+});
+
 // creates a new poll in the polls collection 
 app.post('/polls', async (req, res) =>{
     const newPoll = new Poll(req.body);
     // save new poll to db
     try{
         await newPoll.save();
-        res.status(200).send(newPoll); 
+        const result = {status: 'Success', data: newPoll}; 
+        res.status(200).send(result); 
     }catch(e){
-        res.status(500).send(e);  
+        res.status(500).send({status: 'Error'});  
     }
 
 });
@@ -28,14 +38,16 @@ app.patch('/polls/:id/comments', async (req, res) =>{
     try{
         const updates = req.body; 
         const poll = await Poll.findByIdAndUpdate({_id: req.params.id}, updates);
+        const result = {status: 'Success', data: poll};   
         // note that sending this poll will not be the updated poll, it will be the poll prior to the updates
         // which is something to watch out for while coding the front end 
-        res.status(200).send(poll);  
+        res.status(200).send(result);  
     }catch(e){
-        res.status(500).send(e); 
+        console.log("error in posting comment on poll", e); 
+        res.status(500).send({status: 'Error'});  
     }
 }); 
-
+// udpates votes on a poll 
 app.patch('/polls/:id/votes', async (req, res) =>{
     try{
         // same code as above so obviously can be wrapped into its own function 
